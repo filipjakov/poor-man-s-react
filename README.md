2aab8c259313d5a8f8d2f2bc6e028fe9a3dfa5d8
Filip Jakov Bulić <filip.jakov.bulic@infinum.hr>
2020-05-08 22:52:32 +0200

    feat: suspense

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Poor man's React
+
+## Deconstructing React
+
+1. VDOM
+2. Naive hooks implementation
+3. Naive suspense implementation
+
+Inspired by this [talk](https://youtu.be/f2mMOiCSj5c).
+
+## How tu run
+
+1. Run `npm i`
+2. Run `npm run serve`
+3. Visit `http://localhost:1234`
+4. Play around
--- index.tsx
+++ index.tsx
@@ -1,43 +1,105 @@
 const React = {
-  // VDOM
+  // 1. VDOM
   createElement: (tag, props, ...children) => {
     if (typeof tag === 'function') {
-      return tag(props);
+      // Crutial part for suspense
+      try {
+        return tag(props);
+      } catch ({ promise, key }) {
+        promise.then(data => {
+          promiseCache.set(key, data);
+          reRender();
+        })
+        return { tag: 'h1', props: { children: ['I AM LOADING'] } }
+      }
     }
     const element = { tag, props: { ...props, children } };
-    console.log(element);
     return element;
   }
 };
 
-const App = () => (
-  <div className="react-2020">
-    <h1>Hello, person</h1>
-    <input type="text" placeholder="name"></input>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam non doloremque officiis necessitatibus architecto cumque commodi expedita? Porro deserunt laboriosam officia delectus! Accusantium cum minima, eligendi fugit deleniti unde ex vitae quae nesciunt quam soluta ab at, harum consequatur consectetur?</p>
-  </div>
-);
+// 2. Hooks
+const states = [];
+let currentStateIndex = 0;
+
+const useState = (initialState) => {
+  const FROZENCURSOR = currentStateIndex;
+  states[FROZENCURSOR] = states[FROZENCURSOR] || initialState;
+
+  let setState = newState => {
+    states[FROZENCURSOR] = newState;
+    reRender();
+  }
+  currentStateIndex++;
+
+  return [states[FROZENCURSOR], setState];
+}
+
+// 3. Suspense
+const promiseCache = new Map();
+
+const createResource = (promiseRetrieval, key) => {
+  if (promiseCache.has(key)) {
+    return promiseCache.get(key);
+  }
+
+  throw { promise: promiseRetrieval(), key};
+}
+
+const App = () => {
+  const [name, setName] = useState("person");
+  const [count, setCount] = useState(0);
+
+  const url = createResource(() =>
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(r => r.json())
+      .then(payload => payload.message),
+    'dog photo'
+  );
+
+  return (
+    <div className="react-2020">
+      <h1>Hello, {name}</h1>
+      <input type="text" placeholder="name" value={name} onchange={e => setName(e.target.value)}/>
+      <h2>The count is: {count}</h2>
+      <button onclick={() => setCount(count + 1)}>+</button>
+      <button onclick={() => setCount(count - 1)}>-</button>
+      <br/>
+      <img src={url} width="300" height="300"/>
+    </div>
+  )
+};
 
 const render = (reactElement, container) => {
+  // Stop if current element should be text node
   if (['string', 'number'].includes(typeof reactElement)) {
-    const node = document.createTextNode(String(reactElement));
-    container.appendChild(node);
+    container.appendChild(document.createTextNode(String(reactElement)));
     return;
   }
 
-  const actualDOMElement = document.createElement(reactElement.tag);
+  const { props, tag } = reactElement;
+  const node = document.createElement(tag);
 
+  // If any, assign properties
   if (reactElement.props) {
-    Object.keys(reactElement.props)
+    Object.keys(props)
       .filter(p => p !== 'children')
-      .forEach(p => actualDOMElement[p] = reactElement.props[p]);
+      .forEach(p => node[p] = props[p]);
   }
 
-  if (reactElement.props.children) {
-    reactElement.props.children.forEach(child => render(child, actualDOMElement));
+  // Repeat recursevely for children
+  if (props.children) {
+    props.children.forEach(child => render(child, node));
   }
 
-  container.appendChild(actualDOMElement);
+  container.appendChild(node);
 };
 
+const reRender = () => {
+  currentStateIndex = 0; // Reset hooks 
+  document.querySelector('#app').firstChild.remove();
+  render(<App/>, document.querySelector('#app'));
+}
+
+// Bootstrap
 render(<App/>, document.querySelector('#app'));
