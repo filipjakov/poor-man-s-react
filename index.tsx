12ca2e9d61ef530d1795986939027af4841d9610
Filip Jakov Bulić <filip.jakov.bulic@infinum.hr>
2020-05-09 13:17:44 +0200

    feat: add types

--- index.tsx
+++ index.tsx
@@ -1,44 +1,53 @@
+type Tag = string | Function;
+type Prop = { children: Array<ReactElement>, [x: string]: any };
+type Child = Record<string, string> | string;
+
+type ReactElement = string | number | { tag: Exclude<Tag, Function>; props: Prop};
+
 const React = {
   // 1. VDOM
-  createElement: (tag, props, ...children) => {
+  createElement: (tag: Tag, props: Prop[] | null, ...children: Child[]) => {
+    // On bootstrap, the tag will be a function (App())
     if (typeof tag === 'function') {
       // Crutial part for suspense
       try {
         return tag(props);
       } catch ({ promise, key }) {
-        promise.then(data => {
+        promise.then((data: any) => {
           promiseCache.set(key, data);
           reRender();
         })
-        return { tag: 'h1', props: { children: ['I AM LOADING'] } }
+        return { tag: 'h1', props: { children: ['I AM LOADING'] } }; // Default while fetching the promised value
       }
     }
-    const element = { tag, props: { ...props, children } };
-    return element;
+
+    return { tag, props: { ...props, children } };
   }
 };
 
 // 2. Hooks
-const states = [];
+const states: Array<any> = [];
 let currentStateIndex = 0;
 
-const useState = (initialState) => {
-  const FROZENCURSOR = currentStateIndex;
-  states[FROZENCURSOR] = states[FROZENCURSOR] || initialState;
+function useState<T>(initialState: T): [T, (x: T) => void] {
+  // WITH THE POWER OF CLOSUREEEEE
+  const frozenStateIndex = currentStateIndex;
+  states[frozenStateIndex] = states[frozenStateIndex] || initialState;
 
-  let setState = newState => {
-    states[FROZENCURSOR] = newState;
+  function setState(newState: T): void {
+    states[frozenStateIndex] = newState;
     reRender();
   }
+
   currentStateIndex++;
 
-  return [states[FROZENCURSOR], setState];
+  return [states[frozenStateIndex], setState];
 }
 
 // 3. Suspense
-const promiseCache = new Map();
+const promiseCache = new Map<string, any>();
 
-const createResource = (promiseRetrieval, key) => {
+function createResource<T>(promiseRetrieval: () => Promise<T>, key: string): Promise<T> {
   if (promiseCache.has(key)) {
     return promiseCache.get(key);
   }
@@ -50,29 +59,32 @@ const App = () => {
   const [name, setName] = useState("person");
   const [count, setCount] = useState(0);
 
-  const url = createResource(() =>
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(r => r.json())
-      .then(payload => payload.message),
+  const url = createResource(
+    () => fetch('https://dog.ceo/api/breeds/image/random').then(r => r.json()).then(payload => payload.message),
     'dog photo'
   );
 
   return (
     <div className="react-2020">
       <h1>Hello, {name}</h1>
-      <input type="text" placeholder="name" value={name} onchange={e => setName(e.target.value)}/>
+      <input type="text" placeholder="name" value={name} onchange={(e: Event) => setName((e.target as HTMLInputElement).value)}/>
       <h2>The count is: {count}</h2>
       <button onclick={() => setCount(count + 1)}>+</button>
       <button onclick={() => setCount(count - 1)}>-</button>
       <br/>
       <img src={url} width="300" height="300"/>
+      <div>
+        <div>hello</div>
+      </div>
     </div>
   )
 };
 
-const render = (reactElement, container) => {
+
+const render = (reactElement: ReactElement, container: HTMLElement) => {
+  console.log(reactElement);
   // Stop if current element should be text node
-  if (['string', 'number'].includes(typeof reactElement)) {
+  if (typeof reactElement === 'string' || typeof reactElement === 'number') {
     container.appendChild(document.createTextNode(String(reactElement)));
     return;
   }
